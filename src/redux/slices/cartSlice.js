7f8f9ee9acf8ecdb4b4782875a882447cfbfef13
Sleepwalkerqwe <anatik2005@gmail.com>
2025-03-27 11:25:42 +0100

    made redux logic for cart, show/hide popup-sort

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,21 +1,55 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categoryId: 0,
-  currentPage: 1,
-  sort: { name: 'популярности', sortProperty: 'rating' },
+  totalPrice: 0,
+  items: [],
 };
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    setCategoryId(state, action) {
-      state.categoryId = action.payload;
+    addPizza(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({ ...action.payload, count: 1 });
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+      if (findItem) findItem.count--;
+
+      state.items = state.items.filter((obj) => obj.count > 0);
+
+      // if (state.items.count === 0) state.items = [];
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
+    removePizza(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
+    clearPizza(state) {
+      state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
-export const { setCategoryId, setSort, setCurrentPage, setFilters } = cartSlice.actions;
+export const { addPizza, removePizza, minusItem, clearPizza } = cartSlice.actions;
 
 export default cartSlice.reducer;
